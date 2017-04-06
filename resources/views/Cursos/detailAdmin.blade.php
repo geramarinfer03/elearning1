299aceea8e2353be3052b0fea46e8f8704e29096
Josue11VR <josvr0511@gmail.com>
2017-04-05 18:38:50 -0600

    Cambios interfaz

---
+++ resources/views/Cursos/detailAdmin.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.principal') @section('contenido')
+<section class="content-header">
+	<h1>
+		Programacionn web
+		<small></small>
+	</h1>
+	
+</section>
+
+<br/>
+<br/>
+<section class="content">
+	<div class="row">
+		<div class="box box-primary">
+			<div class="box-header">
+				<i class="ion ion-clipboard"></i>
+
+				<h3 class="box-title">To Do List</h3>
+
+			</div>
+
+		</div>
+
+		<div class="box box-primary">
+			<div class="box-header">
+				<i class="ion ion-clipboard"></i>
+
+				<h3 class="box-title">To Do List</h3>
+
+			</div>
+
+		</div>
+
+		<div class="box box-primary">
+			<div class="box-header">
+				<i class="ion ion-clipboard"></i>
+
+				<h3 class="box-title">To Do List</h3>
+
+			</div>
+
+		</div>
+
+		<div class="box box-primary">
+			<div class="box-header">
+				<i class="ion ion-clipboard"></i>
+
+				<h3 class="box-title">To Do List</h3>
+
+			</div>
+
+		</div>
+	</div>
+
+</section>
+
+@endsection
