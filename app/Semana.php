4b678fe666fe82d1f03933d8c5189b00788f86bb
geramarinfer03 <geramarinfer03@gmail.com>
2017-04-06 21:29:57 -0600

    Modelo semana con foreig key

--- app/Semana.php
+++ app/Semana.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Semana extends Model
 {
-    //
+    protected $fillable = [
+        'id_semana','tema', 'visible', 'estado', 'curso', 'secuencia',
+    ];
+
+    protected $table = "Semana";
+    protected $primaryKey = 'id_semana';
+    public $timestamps = false;
+
+    public function curso_semana(){
+    	return $this->belongsTo(Curso::class,'curso');
+    }
+
 }
