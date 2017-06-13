5fe2e2b341f03f4fa49c3fd5d60eec81f2394d41
Josue Chinchilla <josuech28@gmail.com>
2017-06-13 15:24:01 -0600

    Trigger Actualiza nota entrega

--- Script_BD/TriggerActualizarNotaEntregaV2.sql
+++ Script_BD/TriggerActualizarNotaEntregaV2.sql
@@ -10,26 +10,23 @@ BEGIN
    -- variable declarations --
    DECLARE vNota float(4,2);
    DECLARE countColaboracion int;
-   DECLARE countAutoevaluacion int;
    DECLARE vNotaColaboracion float(4,2);
    DECLARE vNotaAutoevaluacion float (4,2);
    
    -- find Nota Entrega by Id --
    SELECT nota INTO vNota FROM entrega WHERE id_entrega = NEW.id_entrega;
-   SELECT COUNT(id_colaboracion) INTO countColaboracion  FROM colaboracion WHERE id_tipo_colaboracion = 1;
-   SELECT COUNT(id_colaboracion) INTO countAutoevaluacion  FROM colaboracion WHERE id_tipo_colaboracion = 2;
-   SELECT SUM(nota) INTO vNotaColaboracion FROM colaboracion WHERE id_entrega = NEW.id_entrega AND id_tipo_colaboracion = 1;
-   SELECT SUM(nota) INTO vNotaAutoevaluacion FROM colaboracion WHERE id_entrega = NEW.id_entrega AND id_tipo_colaboracion = 2 ;
+   SELECT COUNT(id_colaboracion) INTO countColaboracion  FROM colaboracion WHERE id_tipo_colaboracion = 2;
+   SELECT SUM(nota) INTO vNotaColaboracion FROM colaboracion WHERE id_entrega = NEW.id_entrega AND id_tipo_colaboracion = 2;
+   SELECT nota INTO vNotaAutoevaluacion FROM colaboracion WHERE id_entrega = NEW.id_entrega AND id_tipo_colaboracion = 1;
    
    -- 
    SET @cNota := vNota;
-   -- update nota de la Entrega Autoevaluacion  --
+   -- update nota de la Entrega Colaborcion  --
    IF(NEW.id_tipo_colaboracion > 1) THEN
 		SET @cNotaColaboracion := (vNotaColaboracion / countColaboracion);
 		SET @cNota := @cNota + ((@cNotaColaboracion * 20) / 100);
 	ELSE
-		SET @cNotaAutoevaluacion := (vNotaAutoevaluacion / countAutoevaluacion);
-		SET @cNota := @cNota + ((@cNotaAutoevaluacion * 10) / 100);
+		SET @cNota := @cNota + ((vNotaAutoevaluacion * 10) / 100);
 	END IF;
    UPDATE entrega
 	SET nota= @cNota
