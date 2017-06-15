22da95b33fa0788c8062a99aee7923e8183f3ddc
geramarinfer03 <geramarinfer03@gmail.com>
2017-06-14 23:07:47 -0600

    Revert "Tareas"

--- app/Tarea.php
+++ app/Tarea.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace elearning1;
-use DateTime;
 
 use Illuminate\Database\Eloquent\Model;
 use elearning1\Formulario;
@@ -20,26 +19,6 @@ class Tarea extends Model
     protected $primaryKey = 'id_tarea';
     protected $table = "Tarea";
     public $timestamps = false;
-
-   public function recurso(){
-
-        return $this->belogsTo(Recurso::class, 'id_recurso');
-
-    }
-
-    public function fech_limit_entrega(){
-      $fecha=date("d- M -Y",strtotime($this->fech_limit_entrega)); 
-        return $fecha;
-    }
-
-     public function fech_limit_evaluacion(){
-      $fecha=date("d- M -Y",strtotime($this->fech_limit_evaluacion)); 
-        return $fecha;
-    }
-
-
-
-
 
    
 }
--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'elearning2'),
+            'database' => env('DB_DATABASE', 'elearning'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
             'charset' => 'utf8mb4',
