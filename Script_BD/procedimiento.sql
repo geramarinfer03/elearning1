e58c3e111904e7f26fa0be436f1082ba0c086f17
Josue11VR <josvr0511@gmail.com>
2017-06-15 19:00:20 -0600

    SP desarrollado por Roller y Osvaldo

---
+++ Script_BD/procedimiento.sql
@@ -0,0 +1,47 @@
+USE elearning2;
+
+CREATE TABLE Process_Config ( 
+	Id_Process INT NOT NULL AUTO_INCREMENT ,
+	Hora_Ejecucion TIME NOT NULL ,
+	Fecha_Actualizacion DATETIME NOT NULL , 
+	PRIMARY KEY (Id_Process) 
+) ENGINE = InnoDB
+
+INSERT INTO process_config (Id_Process,	Hora_Ejecucion,	Fecha_Actualizacion) VALUES (NULL, '01:00:00', '2017-06-15 00:00:00');
+
+DROP PROCEDURE IF EXISTS pr_update_process_config;
+DELIMITER $$
+CREATE PROCEDURE pr_update_process_config
+(
+	IN p_Hora_Ejecucion       TIME
+)
+ BEGIN
+  	START TRANSACTION;
+    	SET AUTOCOMMIT = 0;
+		
+		UPDATE	process_config
+		SET		Hora_Ejecucion = p_Hora_Ejecucion,
+				Fecha_Actualizacion = CURDATE()
+		WHERE	Id_Process = 1
+	COMMIT;
+ END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS pr_update_generate_certificate;
+DELIMITER $$
+CREATE PROCEDURE pr_update_generate_certificate()
+ BEGIN
+  	START TRANSACTION;
+    	SET AUTOCOMMIT = 0;
+		
+		UPDATE	Matricula m 
+				INNER JOIN Curso c ON
+					m.Curso = c.Id_Curso
+		SET		m.generarPDF = 1,
+				m.fecha_creado = CURDATE()
+		WHERE	m.promedio_final > 0
+				AND m.generarPDF = 0
+				AND c.Fecha_Final <= CURDATE();
+	COMMIT;
+ END $$
+DELIMITER ;
